e3e542ac3e15419072936d1fedc6653ef3fdd561
Maximilian <ehbauermax@yahoo.de>
2018-02-14 01:20:40 +0100

    Grid is shown correctly

--- src/view.js
+++ src/view.js
@@ -40,28 +40,31 @@ function View() {
     this.yMargin = Math.round((window.innerHeight - this.gameHeight) / 2);
 
     document.getElementsByTagName("body")[0].innerHTML =
+      "<canvas id='game-field-bg' width='" + this.gameWidth + "' height='" + this.gameHeight + "'></canvas>" +
       "<canvas id='game-field' width='" + this.gameWidth + "' height='" + this.gameHeight + "'></canvas>";
 
     this.canvas = document.getElementById("game-field");
-    this.canvas.setAttribute("style", "margin:" + this.xMargin + "px " + this.yMargin + "px");
+    this.canvas.setAttribute("style", "left:" + this.xMargin + "px; right: " + this.xMargin + "px; top:" + this.yMargin);
 
-    this.paintGame();
-  }
+    this.canvasBG = document.getElementById("game-field-bg");
+    this.canvasBG.setAttribute("style", "left:" + this.xMargin + "px; right: " + this.xMargin + "px");
+    this.canvasBG.setAttribute("style", "left:" + this.xMargin + "px; right: " + this.xMargin + "px; top:" + this.yMargin);
 
-  this.paintGame = function() {
     this.ctx = this.canvas.getContext("2d");
-    this.ctx.strokeStyle="#888";
+    this.ctxBG = this.canvasBG.getContext("2d");
+
+    this.ctxBG.strokeStyle="#888";
 
     for (let i = 0; i < this.columns; i++) {
-      this.ctx.moveTo(i * this.fieldSize + 0.5, 0);
-      this.ctx.lineTo(i * this.fieldSize + 0.5, this.gameHeight);
-      this.ctx.stroke();
+      this.ctxBG.moveTo(i * this.fieldSize + 0.5, 0);
+      this.ctxBG.lineTo(i * this.fieldSize + 0.5, this.gameHeight);
+      this.ctxBG.stroke();
     }
 
     for (let i = 0; i < this.rows; i++) {
-      this.ctx.moveTo(0, i * this.fieldSize + 0.5);
-      this.ctx.lineTo(this.gameWidth, i * this.fieldSize + 0.5);
-      this.ctx.stroke();
+      this.ctxBG.moveTo(0, i * this.fieldSize + 0.5);
+      this.ctxBG.lineTo(this.gameWidth, i * this.fieldSize + 0.5);
+      this.ctxBG.stroke();
     }
   }
 
